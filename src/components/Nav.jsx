298050e6f8d2d89091425c955556a27998b7b77b
elhalj <wilsonikedakoffi7@gmail.com>
2025-01-22 17:01:36 +0000

    new

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+      <header className='bg-black h-'>
+        <nav>
+            <div className='bg-slate-100'><h1>logo</h1></div>
+            <div></div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Nav
