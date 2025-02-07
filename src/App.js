387bf19bc99041b94853ab7e4f2582c5de7ee360
elhalj <wilsonikedakoffi7@gmail.com>
2025-02-07 23:54:38 +0000

    speed fonction

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Blog from "./pages/Blog";
 import { injectSpeedInsights } from "@vercel/speed-insights";
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  injectSpeedInsights()
+  injectSpeedInsights();
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element= {<Main />} />
-        <Route path="Blog" element= {<Blog />}/>
+        <Route path="/" element={<Main />} />
+        <Route path="Blog" element={<Blog />} />
       </Routes>
       <Footer />
+      <SpeedInsights />
+      <Analytics />
     </>
   );
 }
