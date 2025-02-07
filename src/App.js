0dfec6be971adf6a07ae2da632ac4d2135a08463
elhalj <wilsonikedakoffi7@gmail.com>
2025-02-07 23:40:02 +0000

    speed fonction

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Blog from "./pages/Blog";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 
 function App() {
+  injectSpeedInsights()
   return (
     <>
       <Header />
