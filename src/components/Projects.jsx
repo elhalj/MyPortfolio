8163a8c6fa3b72c47c2cc553449ff90c50bcde13
elhalj <wilsonikedakoffi7@gmail.com>
2025-06-17 11:07:38 +0000

    add new projetc

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import ecommerce from '../assets/path/to/ecommerce.png';
-import moonchat from '../assets/path/to/moonchat.png';
-import blog from '../assets/path/to/Blog.png';
+import ecommerce from '../assets/path/to/ecommerce.webp';
+import moonchat from '../assets/path/to/moonchat_1.webp';
+import blog from '../assets/path/to/Blog.webp';
+import seguikro from '../assets/path/to/seguikro.webp'
 
 const projects = [
   {
@@ -9,36 +10,50 @@ const projects = [
     description: 'Une plateforme de commerce électronique complète avec un design moderne.',
     image: ecommerce,
     link: 'https://fresh-fashon.onrender.com',
+    version: '0.0.1'
   },
   {
     title: 'MoonChat',
     description: 'Une application de chat en temps réel pour une communication instantanée.',
     image: moonchat,
     link: 'https://moonchat-fn47.onrender.com',
+    version: '0.0.3'
   },
   {
     title: 'Blog Statique',
     description: 'Un blog personnel simple et rapide, construit avec des technologies modernes.',
     image: blog,
-    link: 'https://posts-seven-red.vercel.app/',
+    link: 'https://posts-seven-red.vercel.app',
+    version: '0.0.8'
+  },
+  {
+    title: 'Financial contribution',
+    description: 'cotization plateforme frontend and more financial contribution.',
+    image: seguikro,
+    link: 'https://seguikro.vercel.app/',
+    version: '0.0.2'
   },
 ];
 
-function ProjectCard({ title, description, image, link }) {
+function ProjectCard({ title, description, image, link, version }) {
   return (
     <div className="bg-gray-800 rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-cyan-400/20">
       <img src={image} alt={title} className="w-full h-56 object-cover" />
       <div className="p-6">
         <h3 className="text-2xl font-bold text-white mb-2">{title}</h3>
         <p className="text-gray-400 mb-4">{description}</p>
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300"
-        >
-          Voir le projet
-        </a>
+
+        <div className='flex justify-between p-2'>
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300"
+          >
+            Voir le projet
+          </a>
+          <p className='text-cyan-400 uppercase'>version: V{version}</p>
+        </div>
       </div>
     </div>
   );
